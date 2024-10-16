967b66ef8d99fa827426f6a0a745978ba907ec8d
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-16 16:45:51 +0200

    FIX ex.2 - del variable

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -6,8 +6,7 @@ const fourthTeacher = teachers.at(3);
 // console.log(fourthTeacher);
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-const subTeacher = "Patrick";
-teachers[4] = subTeacher;
+teachers[4] = "Patrick";
 // console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
