bd53c975d825cc05c21d948973426d6356fbc044
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-16 14:44:04 +0200

    ADD solution ex. 7

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -33,7 +33,8 @@ teachers.unshift(newFirstTeacher);
 
 // 7. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes("Fabio");
+// console.log(isFabioPresent);
 
 // 8. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
